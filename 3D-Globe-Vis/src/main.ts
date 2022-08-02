e808f1b05dba4f8b4d9f0e9ada832b1aed070814
Lassi <lassi.l.luukkonen@outlook.com>
2022-08-02 18:57:27 +0200

    Test three.js

--- 3D-Globe-Vis/src/main.ts
+++ 3D-Globe-Vis/src/main.ts
@@ -1,6 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
-import { RGBADepthPacking } from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 0.1, 1000)
@@ -25,11 +25,15 @@ scene.add(pointLight)
 const ambientLight = new THREE.AmbientLight(0x858585)
 scene.add(ambientLight)
 
+const controls = new OrbitControls(camera, renderer.domElement)
+
 function animate() {
   requestAnimationFrame(animate)
+  // mesh.rotation.y += 0.01
+  // mesh.rotation.x += 0.005
+
+  controls.update()
   renderer.render(scene, camera)
-  mesh.rotation.y += 0.01
-  mesh.rotation.x += 0.005
 }
 
 animate()
