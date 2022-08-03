0ad90551dc829918039924da79d0ff3b2029cf64
Lassi <lassi.l.luukkonen@outlook.com>
2022-08-03 17:42:09 +0200

    Implement initial COL visualization

--- 3D-Globe-Vis/src/main.ts
+++ 3D-Globe-Vis/src/main.ts
@@ -2,6 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import earth from '../earth.jpg'
+import { RedFormat } from 'three'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 0.1, 1000)
@@ -30,21 +31,52 @@ scene.add(mesh)
 // y=z
 // z=x
 
+// import { Database } from 'sqlite3'
+
+// // Open a SQLite database, stored in the file db.sqlite
+// const db = new Database('db.sqlite')
+
+// // Fetch a random integer between -99 and +99
+// db.all(
+//   'SELECT *',
+//   (_, res) => console.log(res)
+// )
+
+getData()
+async function getData() {
+  const response = await fetch("processed_data.csv") 
+  const data = await response.text()
+  
+  const table = data.split("\n").slice(1)
+  table.forEach(row => {
+    const columns = row.split(",")
+    var lat = columns[0]
+    var lon = columns[1]
+    var COL = columns[2]
+    drawCity(lat, lon, COL)
+  })
+}
 
 
-drawCity(59.911491, 10.757933) // Oslo
-drawCity(60.192059, 24.945831) // Helsinki
-drawCity(0, 0) // 0, 0
-drawCity(51.509865, -0.118092) // London
-drawCity(40.730610, -73.935242) // NYC
-drawCity(37.773972, -122.431297) // San Francisco
-drawCity(-33.865143, 151.209900) // Sydney
-drawCity(47.3667, 8.5500) // Zürich
+
+// drawCity(59.911491, 10.757933) // Oslo
+// drawCity(60.192059, 24.945831) // Helsinki
+// drawCity(0, 0) // 0, 0
+// drawCity(51.509865, -0.118092) // London
+// drawCity(40.730610, -73.935242) // NYC
+// drawCity(37.773972, -122.431297) // San Francisco
+// drawCity(-33.865143, 151.209900) // Sydney
+// drawCity(47.3667, 8.5500) // Zürich
 
 
-function drawCity(lat: number, lon: number) {
+function drawCity(lat: number, lon: number, col: number) {
   var box = new THREE.SphereGeometry(0.01, 10, 10)
-  var material2 = new THREE.MeshBasicMaterial( {color: 0xffffff} )
+  var color_channels = color(col)
+  var red = color_channels[0]
+  var green = color_channels[1]
+  var blue = color_channels[2]
+  var color2 = new THREE.Color( red, green, blue);
+  var material2 = new THREE.MeshBasicMaterial( {color: color2} )
   var cube = new THREE.Mesh( box, material2 )
   var lat = lat
   var lon = lon
@@ -55,25 +87,44 @@ function drawCity(lat: number, lon: number) {
   scene.add(cube)
 }
 
-const box_x = new THREE.BoxGeometry(0.05, 0.05, 0.05)
-const material_x = new THREE.MeshBasicMaterial( {color: 0xff0000} )
-const cube_x = new THREE.Mesh( box_x, material_x )
-cube_x.position.x = 1
+function color(col: number) {
+  var highest_color = [1, 0, 0]
+  var lowest_color = [0, 0, 1]
+  var pos = (col-15)/(120-15)
+  var red = pos*highest_color[0] + (1-pos)*lowest_color[0]
+  var green = pos*highest_color[1] + (1-pos)*lowest_color[1]
+  var blue = pos*highest_color[2] + (1-pos)*lowest_color[2]
+  return [red, green, blue]
+}
 
-const box_y = new THREE.BoxGeometry(0.05, 0.05, 0.05)
-const material_y = new THREE.MeshBasicMaterial( {color: 0x00ff00} )
-const cube_y = new THREE.Mesh( box_y, material_y )
-cube_y.position.y = 1
+// drawAxes()
 
-const box_z = new THREE.BoxGeometry(0.05, 0.05, 0.05)
-const material_z = new THREE.MeshBasicMaterial( {color: 0x0000ff} )
-const cube_z = new THREE.Mesh( box_z, material_z )
-cube_z.position.z = 1
+function drawAxes() {
+  var box_x = new THREE.BoxGeometry(0.05, 0.05, 0.05)
+  var material_x = new THREE.MeshBasicMaterial( {color: 0xff0000} )
+  var cube_x = new THREE.Mesh( box_x, material_x )
+  cube_x.position.x = 1
 
-scene.add(cube_x, cube_y, cube_z)
+  var box_y = new THREE.BoxGeometry(0.05, 0.05, 0.05)
+  var material_y = new THREE.MeshBasicMaterial( {color: 0x00ff00} )
+  var cube_y = new THREE.Mesh( box_y, material_y )
+  cube_y.position.y = 1
 
-const axesHelper = new THREE.AxesHelper( 5 );
-scene.add( axesHelper );
+  var box_z = new THREE.BoxGeometry(0.05, 0.05, 0.05)
+  var material_z = new THREE.MeshBasicMaterial( {color: 0x0000ff} )
+  var cube_z = new THREE.Mesh( box_z, material_z )
+  cube_z.position.z = 1
+
+  scene.add(cube_x, cube_y, cube_z)
+
+  var axesHelper = new THREE.AxesHelper( 5 );
+  scene.add( axesHelper );
+}
+
+
+
+const text = new THREE.TextGeometry( "HELLO" );
+
 
 
 const pointLight = new THREE.PointLight(0xffffff)
--- Data/process_data.py
+++ Data/process_data.py
@@ -1,66 +1,82 @@
-import csv
-import requests
+# import csv
+# import requests
 
-API_KEY = open("API_KEY.txt").read()
-LIMIT = 5
+# API_KEY = open("API_KEY.txt").read()
+# LIMIT = 5
 
-def api_request(city_name, state_code, country_code) -> tuple():
-    if state_code is not None:
-        api_url = "http://api.openweathermap.org/geo/1.0/direct?q="+city_name+","+state_code+","+country_code+"&limit="+str(LIMIT)+"&appid="+ API_KEY
-    else:
-        api_url = "http://api.openweathermap.org/geo/1.0/direct?q="+city_name+","+country_code+"&limit="+str(LIMIT)+"&appid="+ API_KEY
-    response = requests.get(api_url)
-    j = response.json()
-    if len(j) != 1:
-        print("API ERROR: no loction found or more than one location found")
-        return None
-    try:
-        lat, lon = j[0]["lat"], j[0]["lon"]
-    except:
-        print("Problem with lat and lon")
-    return lat,lon
+# def api_request(city_name, state_code, country_code) -> tuple():
+#     if state_code is not None:
+#         api_url = "http://api.openweathermap.org/geo/1.0/direct?q="+city_name+","+state_code+","+country_code+"&limit="+str(LIMIT)+"&appid="+ API_KEY
+#     else:
+#         api_url = "http://api.openweathermap.org/geo/1.0/direct?q="+city_name+","+country_code+"&limit="+str(LIMIT)+"&appid="+ API_KEY
+#     response = requests.get(api_url)
+#     j = response.json()
+#     if len(j) != 1:
+#         print("API ERROR: no loction found or more than one location found")
+#         return None
+#     try:
+#         lat, lon = j[0]["lat"], j[0]["lon"]
+#     except:
+#         print("Problem with lat and lon")
+#     return lat,lon
 
-country_codes = {}
+# country_codes = {}
 
-with open('country_codes.csv', 'r') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        country = row[0].lower()
-        code = row[1]
-        country_codes[country] = code
+# with open('country_codes.csv', 'r') as csvfile:
+#     reader = csv.reader(csvfile)
+#     for row in reader:
+#         country = row[0].lower()
+#         code = row[1]
+#         country_codes[country] = code
 
-# print(country_codes)
+# # print(country_codes)
 
-counter = 0
-data = []
+# counter = 0
+# data = []
 
-with open('cost_by_city.csv', 'r') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        col_index = row[1]
+# with open('cost_by_city.csv', 'r') as csvfile:
+#     reader = csv.reader(csvfile)
+#     for row in reader:
+#         col_index = row[1]
 
-        name = row[0]
-        comma_count = name.count(",")
-        state = None
-        if comma_count == 1:
-            city, country = name.split(", ")
-        elif comma_count == 2:
-            city, state, country = name.split(", ")
-        country_code = country_codes.get(country.lower())
+#         name = row[0]
+#         comma_count = name.count(",")
+#         state = None
+#         if comma_count == 1:
+#             city, country = name.split(", ")
+#         elif comma_count == 2:
+#             city, state, country = name.split(", ")
+#         country_code = country_codes.get(country.lower())
 
-        try:
-            lat, lon = api_request(city, state, country_code)
-        except:
-            print("Problem with lat and lon")
-            continue
-        data_row = [lat, lon, col_index, city, state, country_code]
-        data.append(data_row)
+#         try:
+#             lat, lon = api_request(city, state, country_code)
+#         except:
+#             print("Problem with lat and lon")
+#             continue
+#         data_row = [lat, lon, col_index, city, state, country_code]
+#         data.append(data_row)
         
-        counter += 1
-        # if counter > 1:
-        #     break
+#         counter += 1
+#         # if counter > 1:
+#         #     break
 
-with open("processed_data.csv", "w", newline="") as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerows(data)
+# with open("processed_data.csv", "w", newline="") as csvfile:
+#     writer = csv.writer(csvfile)
+#     writer.writerows(data)
 
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+create_connection(r"processed_data.db")
